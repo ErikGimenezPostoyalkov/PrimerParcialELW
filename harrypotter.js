5681086797f158178be3312a5128987d7dad2c17
ErikGimenezPostoyalkov <149146012+ErikGimenezPostoyalkov@users.noreply.github.com>
2023-11-17 09:30:16 +0100

    Add files via upload

--- harrypotter.js
+++ harrypotter.js
@@ -2,17 +2,17 @@ const HPAPI_BASE_URL = 'https://wizard-world-api.herokuapp.com';
 
 window.onload = async () => {
 
-    const films = await getAllStarWarsFilms();
+    const wizards = await getAllWizards();
   
     const spinnerHtmlElement = document.getElementById('spinner');
     spinnerHtmlElement.remove();
   
-    for (const film of films) {
+    for (const wizard of wizards) {
       const mainHtmlElement = document.getElementById('main');
       const newElement = document.createElement('div');
       newElement.innerHTML = `
-        <h2>${Wizards.FirstName}</h2>
-        <h2>${Wizards.LastName}</h2>
+        <h2>${wizard.firstname}</h2>
+        <h2>${wizard.lastname}</h2>
       `;
       mainHtmlElement.appendChild(newElement);
     }
@@ -21,6 +21,6 @@ window.onload = async () => {
 async function getAllWizards() {
     const response = await fetch(`${HPAPI_BASE_URL}/Wizards`);
     const data = await response.json();
-    return data.results;
+    return data;
 }
 
